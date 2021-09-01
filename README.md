dfa79290f17eddd1735046570ac2c24560b4bef4
Anu-01-rb <anu.01rob@outlook.com>
2021-09-01 19:48:08 +0700

    Impliment maven project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # class and object diff
-
+#
+# This is a part of midterm exam of OOP
+#
+# 63160015 Anurak Yutthanawa
+# 
+# B.Sc. Computer Science, Burapha University
---
+++ src/main/java/com/anurak/classobjmidtermexam/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.anurak.classobjmidtermexam;
+
+/**
+ *
+ * @author anurak
+ */
+public class Main {
+    
+    static Main main = new Main();
+    
+    void welcome(){
+        System.out.println("Hello This is OOP Midterm exam 3.1 java code");
+    }
+    
+    public static void main(String[] args) {
+        main.welcome();
+    }
+}
